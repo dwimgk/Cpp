be84a5a92d39ccc159275f980a6a73eb9e5c5804
dwimgk <damyan.chakarov@gmail.com>
2025-03-17 15:21:29 +0200

    commit refactors

--- leten_semestur_rabota/domashni/zad4/zad4.cpp
+++ leten_semestur_rabota/domashni/zad4/zad4.cpp
@@ -1,55 +1,72 @@
 #include <iostream>
 #include <string.h>
+#include <vector>
 
 using namespace std;
-const int MaxLengthName = 30;
-const int Patients = 200;
+//const int MaxLengthName = 30;
+//const int Patients = 200;
 
-class Date {
-    int thisDay, thisMonth, thisYear;
+//class Date {
+//    int thisDay, thisMonth, thisYear;
+//public:
+//    Date(int = 1, int = 1, int = 2000); //konstruktor
+//    int Day() const;
+//    int Month() const;
+//    int Year() const;
+//    void setDay(int);
+//    void setMonth(int);
+//    void setYear(int);
+//    void print() const;
+//};
+//
+//Date::Date(int d, int m, int y) {
+//    thisDay = d;
+//    thisMonth = m;
+//    thisYear = y;
+//}
+//int Date::Day() const {
+//    return thisDay;
+//}
+//int Date::Month() const {
+//    return thisMonth;
+//}
+//int Date::Year() const {
+//    return thisYear;
+//}
+//void Date::setDay(int d) {
+//    thisDay = d;
+//}
+//void Date::setMonth(int m) {
+//    thisMonth = m;
+//}
+//void Date::setYear(int y) {
+//    thisYear = y;
+//}
+//
+//void Date::print() const {
+//    cout << thisDay << "." << thisMonth << "." << thisYear << endl;
+//}
+
+class Visit {
 public:
-    Date(int = 1, int = 1, int = 2000); //konstruktor
-    int Day() const;
-    int Month() const;
-    int Year() const;
-    void setDay(int);
-    void setMonth(int);
-    void setYear(int);
-    void print() const;
+    string date;
 };
-
-Date::Date(int d, int m, int y) {
-    thisDay = d;
-    thisMonth = m;
-    thisYear = y;
-}
-int Date::Day() const {
-    return thisDay;
-}
-int Date::Month() const {
-    return thisMonth;
-}
-int Date::Year() const {
-    return thisYear;
-}
-void Date::setDay(int d) {
-    thisDay = d;
-}
-void Date::setMonth(int m) {
-    thisMonth = m;
-}
-void Date::setYear(int y) {
-    thisYear = y;
-}
-
-void Date::print() const {
-    cout << thisDay << "." << thisMonth << "." << thisYear << endl;
-}
-
 class Patient {
+public:
     string FName, LName;
+    string bDate;
+    vector<Visit> visits;
+
+    Patient(){}
+    Patient(string FName, string LName, string bDate) {
+        this->FName = FName;
+        this->LName = LName;
+        this->bDate = bDate;
+    }
 };
 int main()
 {
-    std::cout << "Hello World!\n";
+    Patient p1("Damyan", "Chakarov", "13.03.2005");
+    vector<Patient> patients;
+    
 }
