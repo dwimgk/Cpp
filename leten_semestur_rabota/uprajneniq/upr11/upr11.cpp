969b6997ff184f97004ac095faa09399b56cbbda
dwimgk <damyan.chakarov@gmail.com>
2025-05-19 08:55:15 +0300

    commit upr11

---
+++ leten_semestur_rabota/uprajneniq/upr11/upr11.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Book {
+public:
+    vector<string> pages;
+    Book(vector<string> pages) {
+        this->pages = pages;
+    }
+
+    string shortestPage() {
+        string shortest = this->pages[0];
+        for (int i = 1; i < pages.size(); i++)
+        {
+            if (pages[i].length() < shortest.length())
+            {
+                shortest = pages[i];
+            }
+        }
+        return shortest;
+    }
+
+    void operator+ (string newPage) {
+        this->pages.push_back(newPage);
+        return;
+    }
+    
+    operator string() {
+        string full = "";
+        for (int i = 0; i < pages.size(); i++)
+        {
+            full += pages[i];
+            full += "; ";
+        }
+        return full;
+    }
+};
+
+int main()
+{
+    vector<string> bookVector{ "Zdraveite!", "Az sum Damyan Chakarov.", "Ucha vuv VVMU!" };
+
+    Book b(bookVector);
+    string test1Shortest = b.shortestPage();
+    cout << test1Shortest << endl;
+    string test1 = (string)b;
+    cout << test1 << endl;
+    cout << endl;
+
+    b + "Goodbye!";
+    string test2 = (string)b;
+    string test2Shortest = b.shortestPage();
+    cout << test2Shortest << endl;
+    cout << test2 << endl;
+
+    return 0;
+}
